ef949bebd39419693cf2cea0a7d61cffa90fac27
shiva01234-max <boxofficeboys0@gmail.com>
2026-05-29 00:49:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
 import logging
-logging.basicConfig(
-    level=logging.INFO, 
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[logging.StreamHandler()]
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler()])
 logging.getLogger('pyrogram').setLevel(logging.WARNING)
 logging.getLogger('aiohttp').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
-
 import os
 import time
 import asyncio
@@ -17,13 +12,11 @@ try:
 except ImportError:
     ul = False
     pass
-
 from pyrogram import types, Client, StopPropagation
 from pyrogram.handlers import MessageHandler
 from pyrogram.errors import FloodWait
 from aiohttp import web
 from typing import Union, Optional, AsyncGenerator
-
 from web import web_app
 from info import URL, INDEX_CHANNELS, SUPPORT_GROUP, LOG_CHANNEL, API_ID, DATA_DATABASE_URL, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL, ADMINS, SECOND_FILES_DATABASE_URL, FILES_DATABASE_URL
 from utils import temp, get_readable_time, check_premium
@@ -35,20 +28,12 @@ if ul:
 
 class Bot(Client):
     def __init__(self):
-        super().__init__(
-            name='Auto_Filter_Bot',
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            plugins={"root": "plugins"}
-        )
+        super().__init__(name='Auto_Filter_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins={"root": "plugins"})
         self.listeners = {}
         self.add_handler(MessageHandler(self._listener_handler), group=-1)
 
     async def _listener_handler(self, client: Client, message: types.Message):
-        if not message.chat or not message.from_user:
-            return
-        
+        if not message.chat or not message.from_user: return
         listener_id = (message.chat.id, message.from_user.id)
         if listener_id in self.listeners:
             future = self.listeners[listener_id]
@@ -59,21 +44,15 @@ class Bot(Client):
     async def listen(self, chat_id: int, user_id: int, timeout: int = 60) -> Optional[types.Message]:
         future = asyncio.get_event_loop().create_future()
         listener_id = (chat_id, user_id)
-        
         if listener_id in self.listeners:
             old_future = self.listeners[listener_id]
-            if not old_future.done():
-                old_future.cancel()
-                
+            if not old_future.done(): old_future.cancel()
         self.listeners[listener_id] = future
-        
         try:
             message = await asyncio.wait_for(future, timeout)
             return message
-        except asyncio.TimeoutError:
-            return None
-        finally:
-            self.listeners.pop(listener_id, None)
+        except asyncio.TimeoutError: return None
+        finally: self.listeners.pop(listener_id, None)
 
     async def start(self, **kwargs):
         logger.info('Setting up your database, please wait a moment...')
@@ -84,21 +63,17 @@ class Bot(Client):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
-
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
-            try:
-                await self.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
-            except:
-                pass
+            try: await self.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+            except: pass
             os.remove('restart.txt')
-
         temp.BOT = self
         me = await self.get_me()
         temp.ME = me.id
         temp.U_NAME = me.username
-app = web.AppRunner(web_app)
+        app = web.AppRunner(web_app)
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
         await check_premium(self)
@@ -106,15 +81,6 @@ app = web.AppRunner(web_app)
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
             logger.error("Make sure bot admin in LOG_CHANNEL, exiting now")
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
 
 app = Bot()
 app.run()
